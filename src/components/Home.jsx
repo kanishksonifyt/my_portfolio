1a7d2f5e6d0932a57d56044d49ae773d5ecc6e72
kanishksonifyt <kanishksoni_fyt@outlook.com>
2025-11-21 01:05:14 +0530

    udpate

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Navbar from "./Navbar";
 import First3dmodel from "../3D-assets/First3dmodel";
 import Phoneview from "./Phoneview";
 import Computerview from "./Computerview";
@@ -27,14 +26,8 @@ const Home = () => {
   return (
     // FIX: Changed w-[99vw] to w-screen and h-fit to h-screen
     <div className="w-screen h-screen bg-black text-white overflow-hidden cursor-none relative">
-      
       {/* Navbar Container - Absolute to sit on top */}
       <div className="absolute top-0 left-0 w-full z-50 pointer-events-none">
-        {/* Navbar */}
-        <span className="fixed z-50 flex left-1/2 -translate-x-1/2 pointer-events-auto">
-          <Navbar />
-        </span>
-        
         {/* 3D Model */}
         <span className="w-fit max-sm:hidden fixed md:ml-[80%] lg:ml-[80%] z-[9999] flex justify-end items-center px-3 max-md:pt-16 max-md:justify-center pointer-events-auto">
           <First3dmodel />
